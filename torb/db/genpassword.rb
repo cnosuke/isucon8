6d94d607ded990c0f1ba377d18ef4a24a70d8db8
Takashi Kokubun <takashikkbn@gmail.com>
2018-09-16 14:02:54 +0900

    generate password

---
+++ torb/db/genpassword.rb
@@ -0,0 +1,11 @@
+sql = File.read('isucon8q-initial-dataset.sql')
+
+sql.sub!('(id, nickname, login_name, pass_hash) VALUES', '(id, nickname, login_name, pass_hash, password) VALUES')
+sql.gsub!(/\(\d+,[^,]+, "(?<name>[^",]+)",[^,]+\)/) do |str|
+  name = Regexp.last_match[:name]
+  password = "#{name}#{name.reverse}"
+
+  str.sub(/\)\z/, ", \"#{password}\")")
+end
+
+File.write('isucon8q-initial-dataset-password.sql', sql)
