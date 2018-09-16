950129fb86b3c1d219893698faaa9250166e7481
timakin <timaki.st@gmail.com>
2018-09-16 16:50:12 +0900

    getReservationFuck2

--- torb/webapp/go/src/torb/app.go
+++ torb/webapp/go/src/torb/app.go
@@ -399,7 +399,7 @@ func getEventChildren(event *Event, loginUserID int64) error {
 		sIDs = append(sIDs, sheet.ID)
 	}
 
-	rs, err := getReservationFuck(event.ID, sIDs)
+	rs, err := getReservationFuck2(event.ID, sIDs)
 	if err != nil {
 		return err
 	}
@@ -449,6 +449,29 @@ func getReservationFuck(eID int64, sIDs []int64) ([]*Reservation, error) {
 				}
 				return nil, err
 			}
+		rs = append(rs, &reservation)
+	}
+	return rs, nil
+}
+
+
+func getReservationFuck2(eID int64, sIDs []int64) ([]*Reservation, error) {
+	var rs []*Reservation
+	for _, sID := range sIDs {
+		var reservation Reservation
+		err := sq.Select(`*`).From("reservations").
+			Where(sq.Eq{
+				"event_id":    eID,
+				"sheet_id":    sID,
+				"canceled_at": nil,
+			}).GroupBy(`event_id, sheet_id`).OrderBy(`reserved_at DESC`).Limit(1).RunWith(db).QueryRow().
+			Scan(&reservation.ID, &reservation.EventID, &reservation.SheetID, &reservation.UserID, &reservation.ReservedAt, &reservation.CanceledAt)
+		if err != nil {
+			if err == sql.ErrNoRows {
+				continue
+			}
+			return nil, err
+		}
 		rs = append(rs, &reservation)
 	}
 	return rs, nil
